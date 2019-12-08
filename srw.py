07045a0bbd12396d195aff7a257e27894e46b015
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2019-12-08 10:41:51 +0100

    Some reorg in srw

--- srw.py
+++ srw.py
@@ -9,13 +9,16 @@ np.random.seed(19680801)
 
 def generate_walk(nstep: int):
     """
-    Create a random walk path on Cartesian grid
-    nstep: number of steps
+    Create a nstep-random walk path on Cartesian grid
+    walk is a (2, nstep) array with:
+        - first dimension: (x, y) coordinates
+        - second dimension: step index
     """
     walk = np.empty((2, nstep))
 
     # initial position
     walk[:, 0] = np.array((0, 0))
+
     # four possible directions
     directions = ((0, 1), (1, 0), (-1, 0), (0, -1))
 
@@ -26,40 +29,43 @@ def generate_walk(nstep: int):
     return walk
 
 
-def update_lines(istep: int, data: np.ndarray, path, spot):
-    """
-    Update line date with current time step (istep)
-    """
-    # plot all edges from start to current position
-    path.set_data(data[:, :istep+1])
-    # plot only current position
-    spot.set_data(data[:, istep])
+def generate_animation(nstep: int = 50):
+    """return a nstep-random walk animation"""
 
-
-def plot_walk(nstep: int = 50):
-    """plot a random walk animation"""
+    def update_path(istep: int):
+        """
+        Update line date with current time step (istep)
+        """
+        # plot all edges from start to current position
+        path.set_data(walk[:, :istep+1])
+        # plot only current position
+        spot.set_data(walk[:, istep])
 
     walk = generate_walk(nstep)
 
     # Create figure and plot area
     fig, ax = plt.subplots()
     # a continuous line to draw walk path
-    walk_path = ax.plot(walk[0], walk[1])
-    # a round spot to show current position
-    walk_spot = ax.plot(walk[0][0], walk[1][0], 'o')
+    path, = ax.plot(walk[0], walk[1])
+    # a round-symbol spot to show current position
+    spot, = ax.plot(walk[0, 0], walk[1, 0], 'o')
 
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
-    ax.set_title('2D random walk')
+    ax.set_title(f'{nstep}-nstep random walk')
+    ax.set_aspect('equal', 'datalim')
+    ax.grid(True)
 
-    ani = animation.FuncAnimation(fig, update_lines, range(1, nstep),
-                                  fargs=(walk, walk_path[0], walk_spot[0]),
-                                  interval=50,
-                                  blit=False,
-                                  repeat=False)
+    anim = animation.FuncAnimation(fig,
+                                   func=update_path,
+                                   frames=nstep,
+                                   interval=50,
+                                   blit=False,
+                                   repeat=False)
 
-    plt.show()
+    return anim
 
 
 if __name__ == '__main__':
-    plot_walk(100)
+    anim = generate_animation(100)
+    plt.show()
