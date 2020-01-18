1522f3eac86332528317ffd92d6a02f78ffae890
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2020-01-18 14:44:53 +0100

    Add Wasserstein distance

--- earth_movers.py
+++ earth_movers.py
@@ -16,32 +16,35 @@ class EarthMovers:
         self.xs = np.random.random_sample((self.n, self.n))
         # target position
         self.xt = np.random.random_sample((self.n, self.n))
+        self.M = self._compute_loss_matrix()
 
-    def compute_loss_matrix(self):
+    def _compute_loss_matrix(self):
         """Return loss matrix"""
-        # loss matrix
         M = ot.dist(self.xs, self.xt)
         M /= M.max()
         return M
 
-    def create_figure(self, title):
+    def get_wasserstein_distance(self) -> float:
+        """Return Wasserstein_distance"""
+        return ot.emd2(a=[], b=[], M=self.M)
+
+    def create_figure(self, suptitle: str):
         """Create empty figure"""
         fig, ax = plt.subplots()
         ax.set_xlabel('$x$')
         ax.set_ylabel('$y$')
         ax.set_aspect('equal', 'datalim')  # x and y scales are equal
-        ax.set_title(title)
+        fig.suptitle(suptitle)
         return ax
 
     def plot_ot(self):
 
         xs = self.xs
         xt = self.xt
-        M = self.compute_loss_matrix()
-        ax = self.create_figure(title='Source and target distributions')
+        ax = self.create_figure(suptitle='Source and target distributions')
 
         # using a uniform distribution on samples
-        G = ot.emd(a=[], b=[], M=M)
+        G = ot.emd(a=[], b=[], M=self.M)
 
         # inspired by plot2D_samples_mat()
         mx = G.max()
@@ -54,6 +57,8 @@ class EarthMovers:
         ax.plot(xs[:, 0], xs[:, 1], 'ob', label='Source samples')
         ax.plot(xt[:, 0], xt[:, 1], 'or', label='Target samples')
         ax.legend(loc=0)
+        wd = em.get_wasserstein_distance()
+        ax.set_title(f"Wasserstein distance: {wd:f}", fontsize=10)
 
 
 if __name__ == '__main__':
