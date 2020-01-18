d5a954b95f27977aa3fc02370ff186520607f462
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2020-01-18 14:32:14 +0100

    equal aspect ratio for figure

--- earth_movers.py
+++ earth_movers.py
@@ -6,7 +6,6 @@ https://pot.readthedocs.io/en/stable/auto_examples/plot_OT_2D_samples.html
 import numpy as np
 import matplotlib.pyplot as plt
 import ot
-import ot.plot
 
 
 class EarthMovers:
@@ -25,22 +24,39 @@ class EarthMovers:
         M /= M.max()
         return M
 
+    def create_figure(self, title):
+        """Create empty figure"""
+        fig, ax = plt.subplots()
+        ax.set_xlabel('$x$')
+        ax.set_ylabel('$y$')
+        ax.set_aspect('equal', 'datalim')  # x and y scales are equal
+        ax.set_title(title)
+        return ax
+
     def plot_ot(self):
 
+        xs = self.xs
+        xt = self.xt
         M = self.compute_loss_matrix()
+        ax = self.create_figure(title='Source and target distributions')
 
         # using a uniform distribution on samples
-        G0 = ot.emd(a=[], b=[], M=M)
-        ot.plot.plot2D_samples_mat(self.xs, self.xt, G0, c=[.5, .5, 1])
+        G = ot.emd(a=[], b=[], M=M)
 
-        plt.figure(1)
-        plt.plot(self.xs[:, 0], self.xs[:, 1], 'ob', label='Source samples')
-        plt.plot(self.xt[:, 0], self.xt[:, 1], 'or', label='Target samples')
-        plt.legend(loc=0)
-        plt.title('Source and target distributions')
-        plt.show()
+        # inspired by plot2D_samples_mat()
+        mx = G.max()
+        for i in range(xs.shape[0]):
+            for j in range(xt.shape[0]):
+                if G[i, j] / mx > 1e-8:
+                    ax.plot([xs[i, 0], xt[j, 0]], [xs[i, 1], xt[j, 1]],
+                            alpha=G[i, j] / mx, c=[.5, .5, 1])
+
+        ax.plot(xs[:, 0], xs[:, 1], 'ob', label='Source samples')
+        ax.plot(xt[:, 0], xt[:, 1], 'or', label='Target samples')
+        ax.legend(loc=0)
 
 
 if __name__ == '__main__':
     em = EarthMovers(50)
     em.plot_ot()
+    plt.show()
